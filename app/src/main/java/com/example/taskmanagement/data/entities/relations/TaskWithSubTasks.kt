10d09aa744e97c10c12c7285450a7557aeebbd06
Amirreza.dq <amirreza.dq@gmail.com>
2021-12-28 21:41:29 +0330

    [database] -> Create database Relations. :) Create 2 files with One-to-N Relationships, Which includes the following classes: 1- TaskWithSubTasks.kt 2- TaskWithUsers.kt

---
+++ app/src/main/java/com/example/taskmanagement/data/entities/relations/TaskWithSubTasks.kt
@@ -0,0 +1,20 @@
+package com.example.taskmanagement.data.entities.relations
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.example.taskmanagement.data.entities.SubTask
+import com.example.taskmanagement.data.entities.Task
+import com.example.taskmanagement.data.entities.User
+
+data class TaskWithSubTasks(
+
+    @Embedded val task: Task,
+
+    @Relation(
+        parentColumn = "taskTitle",
+        entityColumn = "taskTitle"
+    )
+
+    val subTasks: List<SubTask>
+
+)
---
+++ app/src/main/java/com/example/taskmanagement/data/entities/relations/TaskWithUsers.kt
@@ -0,0 +1,19 @@
+package com.example.taskmanagement.data.entities.relations
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.example.taskmanagement.data.entities.Task
+import com.example.taskmanagement.data.entities.User
+
+data class TaskWithUsers(
+
+    @Embedded val task: Task,
+
+    @Relation(
+        parentColumn = "taskTitle",
+        entityColumn = "taskTitle"
+    )
+
+    val users: List<User>
+
+)
