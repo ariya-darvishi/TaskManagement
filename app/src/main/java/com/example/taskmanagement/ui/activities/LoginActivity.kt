7b99fedbc5b883021c70e413cc8ffba607bd4a87
Amirreza.dq <amirreza.dq@gmail.com>
2021-12-31 10:52:07 +0330

    [splashScreen] -> create splashScreen. :)
    create splash image layout with logo (vector format) and setup it into themes.xml by writhing custom style and setup it on application theme in manifest.xml file.

--- app/src/main/java/com/example/taskmanagement/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/taskmanagement/ui/activities/LoginActivity.kt
@@ -7,6 +7,7 @@ import com.example.taskmanagement.R
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_TaskManagement)
         setContentView(R.layout.activity_login)
     }
 }
