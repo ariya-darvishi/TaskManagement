7f2fdcaeb7ae4fc474d70d48ac055a5b8d12a554
Amirreza.dq <amirreza.dq@gmail.com>
2022-01-01 08:04:39 +0330

    [BottomNavigationView] -> Create and setup BottomNavigationView in "MainActivity".  :)

--- app/src/main/java/com/example/taskmanagement/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagement/ui/activities/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.taskmanagement.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
 import com.example.taskmanagement.R
+import com.example.taskmanagement.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_TaskManagement)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        setupNavigationComponent()
+    }
+
+    private fun setupNavigationComponent() {
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.mainActivityFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val popupMenu = PopupMenu(this,null)
+        popupMenu.inflate(R.menu.menu_bottom_navigation_view)
+        val menu = popupMenu.menu
+        activityMainBinding.bottomNavigationView.setupWithNavController(menu,navController)
+
+
     }
 }
