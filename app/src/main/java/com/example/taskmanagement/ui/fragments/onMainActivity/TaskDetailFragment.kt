11f3e26484d27c08bba419e0f145352d46e81748
Amirreza.dq <amirreza.dq@gmail.com>
2022-01-02 10:54:45 +0330

    [toolbar] -> Create and setup toolbar in "TaskDetailFragment.kt".  :)

--- app/src/main/java/com/example/taskmanagement/ui/fragments/onMainActivity/TaskDetailFragment.kt
+++ app/src/main/java/com/example/taskmanagement/ui/fragments/onMainActivity/TaskDetailFragment.kt
@@ -1,14 +1,46 @@
 package com.example.taskmanagement.ui.fragments.onMainActivity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.taskmanagement.R
 
 class TaskDetailFragment : Fragment(R.layout.fragment_task_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupToolbar(view)
     }
+
+    @SuppressLint("SetTextI18n", "CutPasteId")
+    private fun setupToolbar(view: View) {
+
+        val navController = Navigation.findNavController(view)
+
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+        val toolbarTitle: TextView = toolbar.findViewById(R.id.toolbar_title)
+        val toolbarBackBtn: ImageButton = toolbar.findViewById(R.id.toolbar_back_btn)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            if (destination.id == R.id.taskDetailFragment) {
+
+                toolbarTitle.text = "Task Details"
+
+                toolbarBackBtn.setOnClickListener {
+                    navController.navigate(R.id.homeFragment)
+                }
+            }
+
+        }
+
+    }
+
 
 }
