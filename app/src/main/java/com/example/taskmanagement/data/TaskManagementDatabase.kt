68f51da1b33456037ce457617ae875ecbb841d19
Amirreza.dq <amirreza.dq@gmail.com>
2022-01-06 12:57:07 +0330

    [database] -> change "userImg" type from "String" to "Int" in  "User" model class. :)
    also, change database version number in "TaskManagementDatabase" class.

--- app/src/main/java/com/example/taskmanagement/data/TaskManagementDatabase.kt
+++ app/src/main/java/com/example/taskmanagement/data/TaskManagementDatabase.kt
@@ -12,7 +12,7 @@ import com.example.taskmanagement.data.entities.User
         SubTask::class,
         User::class
     ],
-    version = 2
+    version = 3
 )
 abstract class TaskManagementDatabase : RoomDatabase() {
 
