1d32bb31487d4f499ba5b9e1f0aef54bd7f49c2e
Amirreza.dq <amirreza.dq@gmail.com>
2022-01-09 04:27:15 +0330

    [getStartPage] -> setup "GetStartPageFragment" page. :)

--- app/src/main/java/com/example/taskmanagement/ui/fragments/onLoginActivity/GetStartPageFragment.kt
+++ app/src/main/java/com/example/taskmanagement/ui/fragments/onLoginActivity/GetStartPageFragment.kt
@@ -1,14 +1,31 @@
 package com.example.taskmanagement.ui.fragments.onLoginActivity
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.taskmanagement.R
+import com.example.taskmanagement.databinding.FragmentGetStartPageBinding
 
-class GetStartPageFragment : Fragment(R.layout.fragment_get_start_page) {
+class GetStartPageFragment : Fragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private lateinit var binding: FragmentGetStartPageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_get_start_page, container, false)
+        binding.fragmentGetStart = this
+        return binding.root
+    }
+
+    fun createNewTaskBtnClickListener(view: View){
+        view.findNavController().navigate(R.id.action_getStartPageFragment_to_selectLoginOrSignupPageFragment)
     }
 
 }
